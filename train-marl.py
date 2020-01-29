026aa5e8ea33ca3fd8e71eef59cfe00826cbdb7e
Asif Anwar <tamal2000@gmail.com>
2020-01-29 03:10:18 +0100

    train update

--- train-marl.py
+++ train-marl.py
@@ -1,13 +1,22 @@
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from collections import deque
+from tqdm import tqdm
 import numpy as np
 import time
 import random
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from api import State, util
-#from bots.marl.marl import Bot
+from bots.marl.marl import Bot
+from bots.marl.marl import features
 from itertools import chain
+from bots.rand import rand
+import os.path
 
 
+player=rand.Bot()
 
 DISCOUNT = 0.99
 REPLAY_MEMORY_SIZE = 50000
@@ -19,7 +28,7 @@ MIN_REWARD = -200
 MEMORY_FRACTION = 0.20
 
 # Environment settings
-EPISODES = 50 #20_000
+EPISODES = 100 #20_000
 # Exploration settings
 epsilon = 1  # not a constant, going to be decayed
 EPSILON_DECAY = 0.99975
@@ -42,86 +51,11 @@ if not os.path.isdir('models'):
     os.makedirs('models')
 
 
-def features(state):
-    # type: (State) -> tuple[float, ...]
-    """
-    Extract features from this state. Remember that every feature vector returned should have the same length.
-
-    :param state: A state to be converted to a feature vector
-    :return: A tuple of floats: a feature vector representing this state.
-    """
-
-    feature_set = []
-
-    # Add player 1's points to feature set
-    p1_points = state.get_points(1)
-    # Add player 2's points to feature set
-    p2_points = state.get_points(2)
-    # Add player 1's pending points to feature set
-    p1_pending_points = state.get_pending_points(1)
-    # Add plauer 2's pending points to feature set
-    p2_pending_points = state.get_pending_points(2)
-    # Get trump suit
-    trump_suit = State.get_trump_suit(state)
-    # Add phase to feature set
-    phase = state.get_phase()
-    # Add stock size to feature set
-    stock_size = state.get_stock_size()
-    # Add leader to feature set
-    leader = state.leader()
-    # Add whose turn it is to feature set
-    whose_turn = state.whose_turn()
-    # Add opponent's played card to feature set
-    opponents_played_card = state.get_opponents_played_card()
-
-    ################## You do not need to do anything below this line ########################
-
-    perspective = state.get_perspective()
-
-    # Perform one-hot encoding on the perspective.
-    # Learn more about one-hot here: https://machinelearningmastery.com/how-to-one-hot-encode-sequence-data-in-python/
-    perspective = [card if card != 'U'   else [1, 0, 0, 0, 0, 0] for card in perspective]
-    perspective = [card if card != 'S'   else [0, 1, 0, 0, 0, 0] for card in perspective]
-    perspective = [card if card != 'P1H' else [0, 0, 1, 0, 0, 0] for card in perspective]
-    perspective = [card if card != 'P2H' else [0, 0, 0, 1, 0, 0] for card in perspective]
-    perspective = [card if card != 'P1W' else [0, 0, 0, 0, 1, 0] for card in perspective]
-    perspective = [card if card != 'P2W' else [0, 0, 0, 0, 0, 1] for card in perspective]
-    # Append one-hot encoded perspective to feature_set
-    feature_set += list(chain(*perspective))
-    # Append normalized points to feature_set
-    total_points = p1_points + p2_points
-    feature_set.append(p1_points/total_points if total_points > 0 else 0.)
-    feature_set.append(p2_points/total_points if total_points > 0 else 0.)
-    # Append normalized pending points to feature_set
-    total_pending_points = p1_pending_points + p2_pending_points
-    feature_set.append(p1_pending_points/total_pending_points if total_pending_points > 0 else 0.)
-    feature_set.append(p2_pending_points/total_pending_points if total_pending_points > 0 else 0.)
-    # Convert trump suit to id and add to feature set
-    # You don't need to add anything to this part
-    suits = ["C", "D", "H", "S"]
-    trump_suit_onehot = [0, 0, 0, 0]
-    trump_suit_onehot[suits.index(trump_suit)] = 1
-    feature_set += trump_suit_onehot
-    # Append one-hot encoded phase to feature set
-    feature_set += [1, 0] if phase == 1 else [0, 1]
-    # Append normalized stock size to feature set
-    feature_set.append(stock_size/10)
-    # Append one-hot encoded leader to feature set
-    feature_set += [1, 0] if leader == 1 else [0, 1]
-    # Append one-hot encoded whose_turn to feature set
-    feature_set += [1, 0] if whose_turn == 1 else [0, 1]
-    # Append one-hot encoded opponent's card to feature set
-    opponents_played_card_onehot = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    opponents_played_card_onehot[opponents_played_card if opponents_played_card is not None else 20] = 1
-    feature_set += opponents_played_card_onehot
-
-    # Return feature set
-    return feature_set
-
+# Agent class
 class DQNAgent:
     def __init__(self):
 
-        # Main MODE
+        # Main model
         self.model = self.create_model()
 
         # Target network
@@ -131,24 +65,34 @@ class DQNAgent:
         # An array with last n steps for training
         self.replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
 
+        # Custom tensorboard object
+        #self.tensorboard = ModifiedTensorBoard(log_dir="logs/{}-{}".format(MODEL_NAME, int(time.time())))
+
         # Used to count when to update target network with main network's weights
         self.target_update_counter = 0
 
     def create_model(self):
         model = Sequential()
         model.add(Dense(32, input_shape=(156,)))
-        model.add(Dense(10, activation='softmax'))
+        model.add(Activation('relu'))
+        model.add(Dense(300))
+        model.add(Activation('relu'))
+        model.add(Dense(20, activation='softmax'))
         model.compile(optimizer='rmsprop',
           loss='categorical_crossentropy',
           metrics=['accuracy'])
 
         return model
 
+    # Adds step's data to a memory replay array
+    # (observation space, action, reward, new observation space, done)
     def update_replay_memory(self, transition):
         self.replay_memory.append(transition)
 
+    # Trains main network every step during episode
     def train(self, terminal_state, step):
 
+        # Start training only if certain number of samples is already saved
         if len(self.replay_memory) < MIN_REPLAY_MEMORY_SIZE:
             return
 
@@ -167,8 +111,11 @@ class DQNAgent:
         X = []
         y = []
 
+        # Now we need to enumerate our batches
         for index, (current_state, action, reward, new_current_state, done) in enumerate(minibatch):
 
+            # If not a terminal state, get new q from future states, otherwise set it to 0
+            # almost like with Q Learning, but we use just part of equation here
             if not done:
                 max_future_q = np.max(future_qs_list[index])
                 new_q = reward + DISCOUNT * max_future_q
@@ -184,7 +131,7 @@ class DQNAgent:
             y.append(current_qs)
 
         # Fit on all samples as one batch, log only on terminal state
-        self.model.fit(np.array(X)/255, np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False)
+        self.model.fit(np.array(X), np.array(y), batch_size=MINIBATCH_SIZE, verbose=0, shuffle=False)
 
         # Update target network counter every episode
         if terminal_state:
@@ -197,170 +144,82 @@ class DQNAgent:
 
     # Queries main network for Q values given current observation space (environment state)
     def get_qs(self, state):
-        return self.model.predict(np.array(state).reshape(-1, *state.shape)
+        return self.model.predict(np.array(state).reshape(-1, *state.shape)/255)[0]
 
 
+agent = DQNAgent()
+
+# Iterate over episodes
+for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
+
+    # Update tensorboard step every episode
+    #agent.tensorboard.step = episode
+
+    # Restarting episode - reset episode reward and step number
+    episode_reward = 0
+    step = 1
+
+    # Reset environment and get initial state
+    current_state = State.generate(phase=1)
 
 
-state = State.generate(phase=1)
-print('Random',state)
-feature = features(state)
-numpy_feature = np.asarray(feature)
-print(numpy_feature.shape)
-# Output (156,)
+    # Reset flag and start iterating until episode ends
+    counter = 0
+    done = False
+    #while not done:
+    while not current_state.finished():
 
-'''
+        current_state_features = np.array(features(current_state))
+        # This part stays mostly the same, the change is to query a model for Q values
 
-from api import State, util
-import pickle
-import os.path
-from argparse import ArgumentParser
-import time
-import sys
+        moves = current_state.moves()
+        if np.random.random() > epsilon:
+            # Get action from Q table
+            action = random.choice(moves)
+            option = np.argmax(agent.get_qs(current_state_features))
+            for move in moves:
+                if move[0] == round(option):
+                    action = move
+        else:
+            # Get random action
+            action = random.choice(moves)
 
-# This package contains various machine learning algorithms
-import sklearn
-import sklearn.linear_model
-from sklearn.neural_network import MLPClassifier
-from sklearn.externals import joblib
+        #move = random.choice(moves)
+        ############### find a way to insert move ##########
+        new_state = current_state.next(action)
+        winner, score = new_state.winner()
+        done = current_state.finished()
+        reward = 0
 
-#from bots.rand import rand
-from bots.rdeep import rdeep
+        if winner == 1:
+            reward += score
+        elif winner == 2:
+            reward += score
 
-from bots.ml.ml import features
+        # Transform new continous state to new discrete state and count reward
+        episode_reward += reward
 
-def create_dataset(path, player=rdeep.Bot(), games=20000, phase=1):
+        new_state_features = np.array(features(new_state))
 
-    data = []
-    target = []
+        # Every step we update replay memory and train main network
+        agent.update_replay_memory((current_state_features, action, reward, new_state_features, done))
+        agent.train(done, step)
 
-    # For progress bar
-    bar_length = 30
-    start = time.time()
+        current_state = new_state
+        step += 1
 
-    for g in range(games-1):
+    ep_rewards.append(episode_reward)
+    if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+        average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        #agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
 
-        # For progress bar
-        if g % 10 == 0:
-            percent = 100.0*g/games
-            sys.stdout.write('\r')
-            sys.stdout.write("Generating dataset: [{:{}}] {:>3}%".format('='*int(percent/(100.0/bar_length)),bar_length, int(percent)))
-            sys.stdout.flush()
+        # Save model, but only when min reward is greater or equal a set value
+        if min_reward >= MIN_REWARD:
+            agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
 
-        # Randomly generate a state object starting in specified phase.
-        state = State.generate(phase=phase)
-
-        state_vectors = []
-
-        while not state.finished():
-
-            # Give the state a signature if in phase 1, obscuring information that a player shouldn't see.
-            given_state = state.clone(signature=state.whose_turn()) if state.get_phase() == 1 else state
-
-            # Add the features representation of a state to the state_vectors array
-            state_vectors.append(features(given_state))
-
-            # Advance to the next state
-            move = player.get_move(given_state)
-            state = state.next(move)
-
-        winner, score = state.winner()
-
-        for state_vector in state_vectors:
-            data.append(state_vector)
-
-            if winner == 1:
-                result = 'won'
-
-            elif winner == 2:
-                result = 'lost'
-
-            target.append(result)
-
-    with open(path, 'wb') as output:
-        pickle.dump((data, target), output, pickle.HIGHEST_PROTOCOL)
-
-    # For printing newline after progress bar
-    print("\nDone. Time to generate dataset: {:.2f} seconds".format(time.time() - start))
-
-    return data, target
-
-
-## Parse the command line options
-parser = ArgumentParser()
-
-parser.add_argument("-d", "--dset-path",
-                    dest="dset_path",
-                    help="Optional dataset path",
-                    default="dataset.pkl")
-
-parser.add_argument("-m", "--model-path",
-                    dest="model_path",
-                    help="Optional model path. Note that this path starts in bots/ml/ instead of the base folder, like dset_path above.",
-                    default="model.pkl")
-
-parser.add_argument("-o", "--overwrite",
-                    dest="overwrite",
-                    action="store_true",
-                    help="Whether to create a new dataset regardless of whether one already exists at the specified path.")
-
-parser.add_argument("--no-train",
-                    dest="train",
-                    action="store_false",
-                    help="Don't train a model after generating dataset.")
-
-
-options = parser.parse_args()
-
-if options.overwrite or not os.path.isfile(options.dset_path):
-    create_dataset(options.dset_path, player=rdeep.Bot(), games=20000)
-
-if options.train:
-
-    # Play around with the model parameters below
-
-    # HINT: Use tournament fast mode (-f flag) to quickly test your different models.
-
-    # The following tuple specifies the number of hidden layers in the neural
-    # network, as well as the number of layers, implicitly through its length.
-    # You can set any number of hidden layers, even just one. Experiment and see what works.
-    hidden_layer_sizes = (64, 32)
-
-    # The learning rate determines how fast we move towards the optimal solution.
-    # A low learning rate will converge slowly, but a large one might overshoot.
-    learning_rate = 0.0001
-
-    # The regularization term aims to prevent overfitting, and we can tweak its strength here.
-    regularization_strength = 0.0001
-
-    #############################################
-
-    start = time.time()
-
-    print("Starting training phase...")
-
-    with open(options.dset_path, 'rb') as output:
-        data, target = pickle.load(output)
-
-    # Train a neural network
-    learner = MLPClassifier(hidden_layer_sizes=hidden_layer_sizes, learning_rate_init=learning_rate, alpha=regularization_strength, verbose=True, early_stopping=True, n_iter_no_change=6)
-    # learner = sklearn.linear_model.LogisticRegression()
-
-    model = learner.fit(data, target)
-
-    # Check for class imbalance
-    count = {}
-    for t in target:
-        if t not in count:
-            count[t] = 0
-        count[t] += 1
-
-    print('instances per class: {}'.format(count))
-
-    # Store the model in the ml directory
-    joblib.dump(model, "./bots/ml/" + options.model_path)
-
-    end = time.time()
-
-    print('Done. Time to train:', (end-start)/60, 'minutes.')
-'''
+    # Decay epsilon
+    if epsilon > MIN_EPSILON:
+        epsilon *= EPSILON_DECAY
+        epsilon = max(MIN_EPSILON, epsilon)
